3ed499ea51cf12b384a8653352f410d4bac80c39
leodefaz367 <leodefaz367@gmail.com>
2026-07-12 12:29:43 -0500

    Panel principal conectado a la pantalla de Productos

--- src/main/java/controller/ControladorPanelPrincipal.java
+++ src/main/java/controller/ControladorPanelPrincipal.java
@@ -1,5 +1,8 @@
 package controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
 import model.Usuario;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -13,14 +16,29 @@ public class ControladorPanelPrincipal {
     private Button btnUsuarios;
     @FXML
     private Button btnConfiguracion;
+    @FXML
+    private BorderPane borderPaneMain;
 
     public void inicializarDashboard(Usuario usuarioLogueado) {
         lblBienvenida.setText("Bienvenido, " + usuarioLogueado.getNombre() + " (" + usuarioLogueado.getRol() + ")");
 
-        // Restricción de roles
+
         if (usuarioLogueado.getRol().equals("Cajero") || usuarioLogueado.getRol().equals("Reportes")) {
             btnUsuarios.setVisible(false); // Solo el admin gestiona usuarios
             btnConfiguracion.setVisible(false);
+        }
+    }
+
+    @FXML
+    public void abrirProductos() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/productos.fxml"));
+            Parent vistaProductos = loader.load();
+
+            borderPaneMain.setCenter(vistaProductos);
+        } catch (Exception e) {
+            System.out.println("Error al cargar la pantalla de productos: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
