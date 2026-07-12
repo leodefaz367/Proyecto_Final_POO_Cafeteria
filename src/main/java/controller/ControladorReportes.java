33ee9a7f48a020f7ee7b91cc4a485fd13d7ab80c
Iblamegreg <gregory.araujo@epn.edu.ec>
2026-07-12 16:56:12 -0500

    Implementacion del módulo de reportes (Solo lectura)

--- src/main/java/controller/ControladorReportes.java
+++ src/main/java/controller/ControladorReportes.java
@@ -1,4 +1,25 @@
 package controller;
 
+import dao.ProductoDAO;
+import model.Producto;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 public class ControladorReportes {
+    @FXML
+    private TableView<Producto> tvReportes;
+    @FXML
+    private TableColumn<Producto, String> colNombre;
+    @FXML
+    private TableColumn<Producto, Integer> colStock;
+
+    @FXML
+    public void initialize() {
+        colNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        colStock.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        tvReportes.setItems(FXCollections.observableArrayList(new ProductoDAO().listar()));
+    }
 }
